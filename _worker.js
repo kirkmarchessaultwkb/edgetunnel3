448277853b27108f7861c081d8bbf07f909e70a3
kirkmarchessaultwkb <166847849+kirkmarchessaultwkb@users.noreply.github.com>
2024-12-11 16:37:00 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,6 @@
-name = "your_project_name"
-main_module = "_worker.js"
-workers_dev = true  # 如果你是在开发模式下测试
-compatibility_flags = []
-
-
+export default {
+  async fetch(request) {
+    return new Response('Hello, Cloudflare Worker!');
+  }
+};
 
