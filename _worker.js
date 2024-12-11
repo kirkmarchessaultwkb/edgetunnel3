2a9892413a64303e5a7bf6628992a7c7779a6467
kirkmarchessaultwkb <166847849+kirkmarchessaultwkb@users.noreply.github.com>
2024-12-11 16:42:04 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,6 +1,13 @@
-export default {
-  async fetch(request) {
-    return new Response('Hello, Cloudflare Worker!');
-  }
-};
+// 统一的handleRequest函数
+async function handleRequest(request) {
+  const url = new URL(request.url);
+  const decodedField = decodeURIComponent(url.searchParams.get("field"));
+  return new Response(`Decoded Field: ${decodedField}`, {
+    headers: { "Content-Type": "text/plain" }
+  });
+}
 
+// 其他功能代码
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request));
+});
