0c04398f11e26c9d17d894de64c6e02c58b10c12
kirkmarchessaultwkb <166847849+kirkmarchessaultwkb@users.noreply.github.com>
2024-12-11 16:36:16 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,22 +1,7 @@
-import { Router } from 'https://cdn.skypack.dev/itty-router?min'; // 确保通过CDN引入 itty-router
+name = "your_project_name"
+main_module = "_worker.js"
+workers_dev = true  # 如果你是在开发模式下测试
+compatibility_flags = []
 
-const router = Router();
-
-// 这里是合并后的 handleRequest 函数
-async function handleRequest(request, decodedField = null) {
-    if (decodedField) {
-        // 处理带有 decodedField 的逻辑
-        return new Response('Decoded Field Processing');
-    } else {
-        // 处理其他逻辑
-        return new Response('Normal Processing');
-    }
-}
-
-router.get('/', handleRequest);
-
-addEventListener('fetch', event => {
-    event.respondWith(router.handle(event.request));
-});
 
 
